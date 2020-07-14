5e9c386ba2a727c1e96b354deba54698ccef44e8
Aviral Omar <aviralomar0301@gmail.com>
2020-07-15 02:32:18 +0530

    start value fixed

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
+
+color1.value = '#ffffff';
+color2.value = '#000000';
 
 function setColors() {
   document.body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    'linear-gradient(to bottom right, ' +
+    color1.value +
+    ', ' +
+    color2.value +
+    ')';
 }
 
-color1.addEventListener("input", setColors);
-color2.addEventListener("input", setColors);
+color1.addEventListener('input', setColors);
+color2.addEventListener('input', setColors);
+
+setColors();
